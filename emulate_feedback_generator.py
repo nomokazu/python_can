0b36035dd3ad204237c3bb221513ad64aef50bef
nomotokazuki@nsl.cs.waseda.ac.jp <nomotokazuki@nsl.cs.waseda.ac.jp>
2023-03-20 11:28:37 +0900

    emulate_handle.py : ハンドル操作をPC上からエミュレーションする, emulate_feedback_generator.py : pixkitの車両情報をエミュレーションする

---
+++ emulate_feedback_generator.py
@@ -0,0 +1,95 @@
+import can
+from python_can import msg
+import time
+import threading
+
+# bus1 = can.interface.Bus('test', bustype='virtual', preserve_timestamps=True)
+# bus2 = can.interface.Bus('test', bustype='virtual')
+bus = can.Bus('http://localhost:54701/', bustype='remote', bitrate=500000, receive_own_messages=True)
+
+counter = 0
+
+command_Steer_AngleTarget = None
+command_Brake_Pedal_Target = None
+command_Drive_ThrottlePedalTarget = None
+
+command_Steer_AngleSpeed = None
+
+timer = 0
+
+can_msg_steering_Report = None
+can_msg_brake_Report = None
+can_msg_throttle_Report = None
+
+def can_sender():
+    global bus
+    global can_msg_steering_Report
+    global can_msg_brake_Report
+    global can_msg_throttle_Report
+
+    while True:
+        if can_msg_steering_Report != None and can_msg_brake_Report != None and can_msg_throttle_Report != None:
+            bus.send(can_msg_steering_Report)
+            bus.send(can_msg_brake_Report)
+            bus.send(can_msg_throttle_Report)
+            time.sleep(0.01)
+
+if __name__ == "__main__":
+    thread1 = threading.Thread(target=can_sender)
+    thread1.start()
+
+    while True:
+        counter = counter + 1
+        receiveCanMsg = bus.recv()
+
+        # コマンドの受信関連処理
+        if receiveCanMsg.arbitration_id == 0x102:
+            message2 = msg.Steering_Command()
+            message2.setDataFromCANMessage(receiveCanMsg.data)
+            message2.toData()
+            command_Steer_AngleTarget = message2.Steer_AngleTarget
+            command_Steer_AngleSpeed = message2.Steer_AngleSpeed
+
+        elif receiveCanMsg.arbitration_id == 0x101:
+            message2 = msg.Brake_Command()
+            message2.setDataFromCANMessage(receiveCanMsg.data)
+            message2.toData()
+            command_Brake_Pedal_Target = message2.Brake_Pedal_Target
+        
+        elif receiveCanMsg.arbitration_id == 0x100:
+            message2 = msg.Throttle_Command()
+            message2.setDataFromCANMessage(receiveCanMsg.data)
+            message2.toData()
+            command_Drive_ThrottlePedalTarget = message2.Drive_ThrottlePedalTarget
+        
+        # 受信したコマンドの受信の表示
+        if counter % 100 == 0:
+            counter = 1
+            print("Steer_AngleTarget : " + str(command_Steer_AngleTarget))
+            print("Brake_Pedal_Target : " + str(command_Brake_Pedal_Target))
+            print("Drive_ThrottlePedalTarget : " + str(command_Drive_ThrottlePedalTarget))
+
+        # report メッセージの生成処理
+
+        # すべての command を受信していたら
+        if command_Steer_AngleTarget != None and command_Brake_Pedal_Target != None and command_Drive_ThrottlePedalTarget != None and command_Steer_AngleSpeed != None:
+            steering_Report = msg.Steering_Report()
+            brake_Report = msg.Brake_Report()
+            throttle_Report = msg.Throttle_Report()
+
+            # コマンドの情報から、reportを生成する
+            steering_Report.setDataFromInt(Steer_EnState=1, Steer_Flt1=0, Steer_Flt2=0, Steer_AngleActual=command_Steer_AngleTarget, Steer_AngleRear_Actual=500, Steer_AngleSpeedActual=command_Steer_AngleSpeed)
+            brake_Report.setDataFromInt(Brake_EnState=1, Brake_Flt1=0, Brake_Flt2=0, Brake_PedalActual=command_Brake_Pedal_Target)
+            throttle_Report.setDataFromInt(Drive_EnState=1, Drive_Flt1=0, Drive_Flt2=0, Drive_ThrottlePedalActual=command_Drive_ThrottlePedalTarget)
+
+            # CANに変換
+            steering_Report.toData()
+            brake_Report.toData()
+            throttle_Report.toData()
+
+
+            can_msg_steering_Report = can.Message(arbitration_id = steering_Report.msg_id, data= steering_Report.data, is_extended_id = False)
+            can_msg_brake_Report = can.Message(arbitration_id = brake_Report.msg_id, data= brake_Report.data, is_extended_id = False)
+            can_msg_throttle_Report = can.Message(arbitration_id = throttle_Report.msg_id, data= throttle_Report.data, is_extended_id = False)
+
+            
---
+++ emulate_handle.py
@@ -0,0 +1,140 @@
+import tkinter as tk
+import os
+import sys
+from python_can import sender
+import threading
+
+steering = 0
+brake_var = 0
+throttle_var = 0
+
+class Application(tk.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+
+        self.master.title("Scaleの作成")     # ウィンドウタイトル
+
+        #---------------------------------------------------------------
+        # Scaleの作成
+
+        # Scale（デフォルトで作成）
+        # scaleV = tk.Scale( self.master)
+        # scaleV.pack(side = tk.RIGHT)
+
+        # Scale（オプションをいくつか設定）
+        l = tk.Label(
+            root,
+            text="左    ステアリング    右",   #表示文字
+            )
+        l.pack()
+        self.scal_steering_var = tk.DoubleVar()
+        scaleH_steering = tk.Scale( self.master, 
+                    variable = self.scal_steering_var, 
+                    command = self.slider_scroll,
+                    orient=tk.HORIZONTAL,   # 配置の向き、水平(HORIZONTAL)、垂直(VERTICAL)
+                    length = 300,           # 全体の長さ
+                    width = 20,             # 全体の太さ
+                    sliderlength = 20,      # スライダー（つまみ）の幅
+                    from_ = -1,            # 最小値（開始の値）
+                    to = 1,               # 最大値（終了の値）
+                    resolution=0.1,         # 変化の分解能(初期値:1)
+                    tickinterval=50         # 目盛りの分解能(初期値0で表示なし)
+                    )
+        scaleH_steering.pack()
+
+        l = tk.Label(
+            root,
+            text="踏んでない<-    ブレーキ    ->踏んでる",   #表示文字
+            )
+        l.pack()
+
+        #---------------------------------------------------------------
+        self.scal_brake_var = tk.DoubleVar()
+        scaleH_brake = tk.Scale( self.master, 
+                    variable = self.scal_brake_var, 
+                    command = self.slider_scroll,
+                    orient=tk.HORIZONTAL,   # 配置の向き、水平(HORIZONTAL)、垂直(VERTICAL)
+                    length = 300,           # 全体の長さ
+                    width = 20,             # 全体の太さ
+                    sliderlength = 20,      # スライダー（つまみ）の幅
+                    from_ = 1,            # 最小値（開始の値）
+                    to = 0,               # 最大値（終了の値）
+                    resolution=0.1,         # 変化の分解能(初期値:1)
+                    tickinterval=50         # 目盛りの分解能(初期値0で表示なし)
+                    )
+        scaleH_brake.pack()
+
+
+        l = tk.Label(
+            root,
+            text="踏んでない<-    スロットル    ->踏んでる",   #表示文字
+            )
+        l.pack()
+
+        self.scal_throttle_var = tk.DoubleVar()
+        scaleH_throttle = tk.Scale( self.master, 
+                    variable = self.scal_throttle_var, 
+                    command = self.slider_scroll,
+                    orient=tk.HORIZONTAL,   # 配置の向き、水平(HORIZONTAL)、垂直(VERTICAL)
+                    length = 300,           # 全体の長さ
+                    width = 20,             # 全体の太さ
+                    sliderlength = 20,      # スライダー（つまみ）の幅
+                    from_ = 1,            # 最小値（開始の値）
+                    to = -1,               # 最大値（終了の値）
+                    resolution=0.1,         # 変化の分解能(初期値:1)
+                    tickinterval=50         # 目盛りの分解能(初期値0で表示なし)
+                    )
+        scaleH_throttle.pack()
+
+
+
+    def slider_scroll(self, event=None):
+        '''スライダーを移動したとき'''
+        print(str(self.scal_steering_var.get()))
+        print(str(self.scal_brake_var.get()))
+        print(str(self.scal_throttle_var.get()))
+
+
+def can_sender():
+    global steering
+    global brake_var
+    global throttle_var
+    # どういった操作を行うかのシナリオの定義
+    emulation_scenrio = {"axi0" : steering, "axi2" : brake_var, "axi3" : throttle_var}
+
+    # ループ
+    active = True
+    handleControllerCommand = sender.HandleControllerCommand()
+    while active:
+
+        # print を入れると、CANの送信処理が正しく走らなくなるのでデバッグ時以外はprint を消す
+        # print('十時キー:', joystick.get_axis(0), joystick.get_axis(1), joystick.get_axis(2), joystick.get_axis(3))
+        
+        # ハンドルは軸0
+        handle = emulation_scenrio["axi0"]
+        
+        # ブレーキペダルが軸3
+        # 全く踏んでいない状態が 1 で、完全に踏むと 0 になる ので、それを
+        # 全く踏んでいない状態 : 0 -> 完全に踏むと 1 に補正している。
+        break_pedal_raw = 1- emulation_scenrio["axi3"]
+        
+        # アクセルペダルが軸2
+        # 全く踏んでいない状態が 1 で、完全に踏むと -1 になるので、それを
+        # 全く踏んでいない状態 : 0 -> 完全に踏むと 1 に補正している。
+        accel_pedal = ((-1) * (emulation_scenrio["axi2"]) + 1 ) / 2
+        brake_pedal = 0
+        
+        # ブレーキペダルがごくわずかにしか踏まれていないときは、踏んでいない状態とするようにする
+        if break_pedal_raw > 0.02:
+            brake_pedal = break_pedal_raw
+        handleControllerCommand.setHandleControllerCommandSend(handle, accel_pedal, brake_pedal)
+        handleControllerCommand.startCanSend()
+        if handleControllerCommand.sendFlag == False:
+            handleControllerCommand.sendFlag = True
+
+if __name__ == "__main__":
+    thread1 = threading.Thread(target=can_sender)
+    thread1.start()
+    root = tk.Tk()
+    app = Application(master = root)
+    app.mainloop()
